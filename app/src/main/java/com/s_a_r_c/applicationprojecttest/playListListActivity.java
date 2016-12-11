e5e6c4c27d01f74bae7895891f0260f7b5ee9d5f
Ben <newhope@live.ca>
2016-12-10 21:53:05 -0500

    no message

--- app/src/main/java/com/s_a_r_c/applicationprojecttest/playListListActivity.java
+++ app/src/main/java/com/s_a_r_c/applicationprojecttest/playListListActivity.java
@@ -143,7 +143,7 @@ public class playListListActivity extends AppCompatActivity implements Navigatio
 
                 TextView textView = (TextView) super.getView(position, convertView, parent);
 
-                Log.e("LISTVIEW COLORS" , playlistsMaps.get(position).owner + "~" + (UserDatabase.getInstance(getApplicationContext()).retournerInfosUser().get(UserDatabase.USER_ID));
+                Log.e("LISTVIEW COLORS" , playlistsMaps.get(position).owner + "~" + (UserDatabase.getInstance(getApplicationContext()).retournerInfosUser().get(UserDatabase.USER_ID)));
                 if (Integer.valueOf(playlistsMaps.get(position).owner) == UserDatabase.getInstance(getApplicationContext()).retournerInfosUser().get(UserDatabase.USER_ID)) {
                     textView.setTextColor(Color.parseColor("#00ff00"));
                 }
